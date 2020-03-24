84736ffed56470825a9955f8905f68e7d19ad568
DMdavydov <dmitry_davydov1@epam.com>
2020-03-24 10:13:05 +0300

    Initial commit

---
+++ src/main/java/com/ddavydov/bugtrackereurekaserver/BugTrackerEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ddavydov.bugtrackereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BugTrackerEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackerEurekaServerApplication.class, args);
+    }
+
+}
